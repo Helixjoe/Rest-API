ff9cc14c58d01b5e344a709474454eae4cf8eae1
Helixjoe <joeljohn5112@gmail.com>
2023-06-30 11:27:07 +0530

    GET,POST and DELETE routes added

--- app.js
+++ app.js
@@ -16,7 +16,29 @@ const articleSchema = {
 
 const Article = mongoose.model("article", articleSchema);
 
+app.get("/articles", (req, res) => {
+    async function find() {
+        const foundItems = await Article.find();
+        res.send(foundItems);
+    };
+    find();
+});
+
+app.post("/articles", (req, res) => {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save();
+})
+
+app.delete("/articles", (req, res) => {
+    async function deleteArticle() {
+        await Article.deleteMany();
+    };
+    deleteArticle();
+});
 
 app.listen("3000", () => {
     console.log("Server started on port 3000");
-};
+});
